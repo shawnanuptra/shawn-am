8f1be8a374af6cba056a20f30646db59a26dd471
Shawn Martin <bh74sa@student.sunderland.ac.uk>
2024-06-25 18:06:11 +0100

    added types to getStaticPaths and getStaticProps

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,4 +1,6 @@
-import { Projects } from "@/utilities/data";
+import { Project, Projects } from "@/utilities/data";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
 import React from "react";
 
 const ProjectPage = ({ project }: any) => {
@@ -8,7 +10,7 @@ const ProjectPage = ({ project }: any) => {
 export default ProjectPage;
 
 // This function gets called at build time
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
 	// Call an external API endpoint to get posts
 	// const res = await fetch("https://.../posts");
 	// const posts = await res.json();
@@ -23,11 +25,17 @@ export async function getStaticPaths() {
 	// We'll pre-render only these paths at build time.
 	// { fallback: false } means other routes should 404.
 	return { paths, fallback: false };
+};
+
+interface IParams extends ParsedUrlQuery {
+	slug: string;
 }
 
-export async function getStaticProps({ params }: any) {
+// export async function getStaticProps(): GetStaticProps {
+export const getStaticProps: GetStaticProps = async (context) => {
+	const { slug } = context.params as IParams;
 	// const res = await fetch(`https://.../projects/${params.id}`);
-	const project = Projects.find((element) => element.slug === params.slug);
+	const project = Projects.find((element) => element.slug === slug);
 
 	return { props: { project } };
-}
+};
