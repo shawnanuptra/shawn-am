6d8410ec1a9424b42ede0045cdf10dc282bcb2b7
shawn <shawnanuptraamartin@gmail.com>
2024-07-16 15:56:48 +0100

    added '/projects' limited Latest Projects in '/' added Link to '/projects'

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,12 @@
+function Projects() {
+    return (
+        <div>
+            projects
+            <ul>
+                <li>asldf</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Projects;
