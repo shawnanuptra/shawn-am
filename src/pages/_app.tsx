458cd4bcb31a159f6198b515787ffda02b84bec3
Shawn Martin <79595769+shawnanuptra@users.noreply.github.com>
2024-11-09 20:37:06 +0000

    removed nextauth from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import Layout from "@/components/layout";
 import { GlobalStyles } from "@/components/styles";
-import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps, session: Session) {
+export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<SessionProvider session={session}>
-			<Layout>
-				<GlobalStyles />
-				<Component {...pageProps} />
-			</Layout>
-		</SessionProvider>
+		<Layout>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</Layout>
 	);
 }
